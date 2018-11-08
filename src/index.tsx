9f091aa49282c771e5acfce5694877189f0818d6
steelydylan <ess_president@me.com>
2018-11-09 00:10:59 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { ModalState, ModalProps } from './types';
+
+export default class Modal extends React.Component<ModalProps, ModalState> {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface ModalProps {
+
+}
+
+export interface ModalState {
+
+}
